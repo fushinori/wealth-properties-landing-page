806d2fd1fd8d6391bb5be3cdfa397ef1ae100032
Rishi <git@fushinori.me>
2023-11-19 22:59:46 +0530

    Add footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Nunito } from "next/font/google";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer";
 import "./globals.css";
 
 const nunito = Nunito({ subsets: ["latin"] });
@@ -13,6 +14,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={nunito.className}>
         <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
